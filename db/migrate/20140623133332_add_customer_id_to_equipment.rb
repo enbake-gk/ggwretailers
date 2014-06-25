89e521bcb6bf5ab4ed0ba3474ba2d3881406bbfe
gkumar@enbake.com <gkumar@enbake.com>
2014-06-25 19:46:57 +0530

    added migration file

--- db/migrate/20140623133332_add_customer_id_to_equipment.rb
+++ db/migrate/20140623133332_add_customer_id_to_equipment.rb
@@ -1,7 +1,5 @@
 class AddCustomerIdToEquipment < ActiveRecord::Migration
   def change
-    add_column :equipment, :customer_id, :integer
-    add_column :equipment, :serial_number, :string
     add_index :equipment, :serial_number, unique: true
   end
 end
