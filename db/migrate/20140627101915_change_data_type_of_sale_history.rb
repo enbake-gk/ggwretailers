1e6e2c6d8e9166130666359ce085c8183a936eef
gkumar@enbake.com <gkumar@enbake.com>
2014-07-04 16:25:35 +0530

    added colour module

--- db/migrate/20140627101915_change_data_type_of_sale_history.rb
+++ db/migrate/20140627101915_change_data_type_of_sale_history.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypeOfSaleHistory < ActiveRecord::Migration
   def change
   	remove_column :sale_histories, :dob
-  	change_column :sale_histories, :dob, :datetime
+  	add_column :sale_histories, :dob, :datetime
   end
 end
