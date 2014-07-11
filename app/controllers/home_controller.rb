1dd601724ed365e9e71681913a3dd4a44627a804
sp-enbake <sprasad@enbake.com>
2014-07-11 10:32:30 +0530

    changes for root url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
 	def index
-		if current_user.is_admin? 	
-			redirect_to brands_path
-		elsif current_user.is_manager? 	
-			redirect_to brands_path
+		if ((current_user.is_admin?) || (current_user.is_manager?)) 		
+			redirect_to serialnumbers_path
 		elsif current_user.is_retailer?
 			redirect_to customers_path
 		else
