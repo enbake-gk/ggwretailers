7e9db4130d13e600c2d862ac341e26a4e9a7ce2e
sp-enbake <sprasad@enbake.com>
2014-07-17 14:39:22 +0530

    add auto fill by email to sales to customer

--- app/controllers/sale_to_customers_controller.rb
+++ app/controllers/sale_to_customers_controller.rb
@@ -62,6 +62,13 @@ class SaleToCustomersController < ApplicationController
     end
   end
 
+  def get_customer
+    @result = Equipment.where(:email => params[:email]).first
+    respond_to do |format|
+        format.js
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_sale_to_customer
